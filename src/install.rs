ecd8c51e467d9d4b96237b5a0afda792b0b4fd91
leon3s <leo.vernisse@gmail.com>
2022-10-15 13:12:08 +0200

    [BUGFIX] Added cockroachdb in installation

--- src/install.rs
+++ src/install.rs
@@ -6,9 +6,12 @@ use crate::errors::DaemonError;
 pub async fn install_components(
   docker: &bollard::Docker,
 ) -> Result<(), DaemonError> {
-  components::utils::install_component("postgres:alpine3.16", docker).await?;
+  components::utils::install_component(
+    "cockroachdb/cockroach:v21.2.16",
+    docker,
+  )
+  .await?;
   components::utils::build_component("nanocl-dns-dnsmasq", docker).await?;
   components::utils::build_component("nanocl-proxy-nginx", docker).await?;
-
   Ok(())
 }
