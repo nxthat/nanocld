e14d43beb06bb6ed77719eb85bc8e7b0e1970545
leon3s <leo.vernisse@gmail.com>
2022-10-26 14:27:19 +0200

    [UPDATE] version.rs

--- src/version.rs
+++ src/version.rs
@@ -1,3 +1,4 @@
 pub const ARCH: &str = "amd64";
-pub const VERSION: &str = "0.1.3";
-pub const COMMIT_ID: &str = "3f65a6b2";
+pub const VERSION: &str = "0.1.4";
+pub const COMMIT_ID: &str = "9a501ab8";
+
