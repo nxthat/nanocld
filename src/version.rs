0f3598a818054f835a3e381249a9c046ff47fb8f
leon3s <leo.vernisse@gmail.com>
2022-11-02 14:33:02 +0100

    [RELEASE] 0.1.6

--- src/version.rs
+++ src/version.rs
@@ -1,4 +1,4 @@
 pub const ARCH: &str = "amd64";
-pub const VERSION: &str = "0.1.5";
-pub const COMMIT_ID: &str = "0a70dd95";
+pub const VERSION: &str = "0.1.6";
+pub const COMMIT_ID: &str = "584b17d7";
 
