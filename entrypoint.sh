50129d92986094569b0cd19aaf83f0793a4e17cd
leone <leo.vernisse@gmail.com>
2022-11-13 19:47:05 +0100

    [REFACTOR] entry point to run as root and nanocl group (#50)

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,16 +4,10 @@ getent group nanocl > /dev/null 2&>1
 
 if [ $? -ne 0 ]; then
   addgroup -S nanocl -g $NANOCL_GID
-fi
-
-getent passwd nanocl > /dev/null 2&>1
-
-if [ $? -ne 0 ]; then
-  adduser -S nanocl -G nanocl -u $NANOCL_UID
-  chown nanocl:nanocl -R /run/nanocl
+  chown root:nanocl -R /run/nanocl
   chmod -R 770 /run/nanocl
 fi
 
 sh -c "sleep 5 && chmod -R 770 /run/nanocl" &
 
-exec runuser -u nanocl -g nanocl -- /usr/local/bin/nanocld $@
+exec runuser -u root -g nanocl -- /usr/local/bin/nanocld $@
