78e1be5de82133d865086ba42cdfcc1e0014d30f
leon3s <leo.vernisse@gmail.com>
2022-11-13 01:57:23 +0100

    [BUGFIX] Forgot to apply version patch

--- scripts/test_musl.sh
+++ scripts/test_musl.sh
@@ -1,3 +1,4 @@
+export RUSTFLAGS='-C target-feature=-crt-static -L/usr/lib/x86_64-linux-musl -L/lib/x86_64-linux-musl -C linker=musl-gcc -Clink-arg=/usr/lib/x86_64-linux-musl/libc.a -Clpq -Clpgport -Clpgcommon'
 export PKG_CONFIG_ALLOW_CROSS=1
 export PKG_CONFIG_ALL_STATIC=true
 export OPENSSL_STATIC=true
--- src/version.rs
+++ src/version.rs
@@ -1,4 +1,4 @@
 pub const ARCH: &str = "amd64";
-pub const VERSION: &str = "0.1.9";
-pub const COMMIT_ID: &str = "11d9289b";
+pub const VERSION: &str = "0.1.10";
+pub const COMMIT_ID: &str = "ad4e0dfe";
 
