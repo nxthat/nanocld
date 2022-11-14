43c5d05a37a720fe2115c5f06060217b945d5aa7
leone <leo.vernisse@gmail.com>
2022-11-14 18:14:25 +0100

    Release 0.1.12 (#54)
    * [BUGFIX] Test not running
    
    * [RELEASE] 0.1.12

--- src/version.rs
+++ src/version.rs
@@ -1,4 +1,4 @@
 pub const ARCH: &str = "amd64";
-pub const VERSION: &str = "0.1.11";
-pub const COMMIT_ID: &str = "50129d92";
+pub const VERSION: &str = "0.1.12";
+pub const COMMIT_ID: &str = "e82ff62f";
 
