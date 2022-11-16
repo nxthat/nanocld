3108e9e52a0c234bdad4750f7755c36af26d3466
leon3s <leo.vernisse@gmail.com>
2022-11-16 22:14:09 +0100

    [WIP] Vpn as Cargo

--- src/controllers/vpn.rs
+++ src/controllers/vpn.rs
@@ -18,10 +18,19 @@ pub async fn register(arg: &ArgState) -> Result<(), DaemonError> {
   }
   let path = Path::new(&arg.config.state_dir).join("store/data");
   let binds = vec![format!("{}:/cockroach/cockroach-data", path.display())];
+  let envs = vec![
+    String::from("VPN_DNS_SRV1=155.0.0.1"),
+    String::from("VPN_PUBLIC_IP=155.0.0.1"),
+    "VPN_L2TP_NET=192.168.84.0/16",
+    "VPN_L2TP_LOCAL=192.168.84.1",
+    "VPN_L2TP_POOL=192.168.84.10-192.168.84.254",
+    "VPN_XAUTH_NET=192.168.85.0/16",
+    "VPN_XAUTH_POOL=192.168.85.10-192.168.85.254",
+  ];
   let store_cargo = CargoPartial {
-    name: String::from("store"),
-    image_name: String::from("cockroachdb/cockroach:v21.2.17"),
-    environnements: None,
+    name: String::from("system-nano-vpn"),
+    image_name: String::from("hwdsl2/ipsec-vpn-server"),
+    environnements: Some(envs),
     binds: Some(binds),
     replicas: Some(1),
     dns_entry: None,
