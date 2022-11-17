b901b64df7345c3608799c3a6c60591fb9ae2726
leon3s <leo.vernisse@gmail.com>
2022-11-17 06:31:35 +0100

    [RELEASE] 0.1.14

--- src/version.rs
+++ src/version.rs
@@ -1,4 +1,4 @@
 pub const ARCH: &str = "amd64";
-pub const VERSION: &str = "0.1.13";
-pub const COMMIT_ID: &str = "68daa2e7";
+pub const VERSION: &str = "0.1.14";
+pub const COMMIT_ID: &str = "1957fc4e";
 
