fbeefc11e96a97adc9cf27ece3058e54d4741af7
leone <leo.vernisse@gmail.com>
2022-11-17 23:50:58 +0100

    [RELEASE] 0.1.17 (#65)
    * [BUGFIX] Controller dns and proxy image again
    
    * [RELEASE] 0.1.17

--- src/version.rs
+++ src/version.rs
@@ -1,4 +1,4 @@
 pub const ARCH: &str = "amd64";
-pub const VERSION: &str = "0.1.16";
-pub const COMMIT_ID: &str = "cc3774d6";
+pub const VERSION: &str = "0.1.17";
+pub const COMMIT_ID: &str = "5fa0a270";
 
